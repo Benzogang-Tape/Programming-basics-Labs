ca5644c68c4eb616dfcf627884a773d4ed0e1773
DJ Tape <Dj.Tapee@yandex.ru>
2023-11-20 04:16:42 +0300

    make function static

---
+++ Lab 4.1/Solution.cpp
@@ -0,0 +1,9 @@
+#include "Solution.h"
+
+auto Solution::solve(double(*method)(double(*)(double), double, double)) {
+	Function::print_equation();
+}
+
+auto Solution::solve2(Function F, Methods M) {
+	Function::print_equation();
+}
