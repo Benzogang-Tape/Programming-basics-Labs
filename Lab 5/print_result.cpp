414b369f36210d8bb7852dcfeb6064d59c4b4dbf
DJ Tape <Dj.Tapee@yandex.ru>
2023-12-18 13:58:13 +0300

    Revert normal version

--- Lab 5/print_result.cpp
+++ Lab 5/print_result.cpp
@@ -1,16 +1,16 @@
 #include "print_result.h"
 
 
-void display_arr(int arr[], unsigned arr_size) {
-	for (unsigned i{}; i < arr_size; i++) {
-		std::cout << ' ' << arr[i];
-	}
+std::string display_arr(int arr[], unsigned arr_size) {
+	std::string result = "";
+	for (unsigned i{}; i < arr_size; i++)
+		result = result + " " + std::to_string(arr[i]);
+	return result;
 }
 
 void print_result(int arr[], int aux_arr[], unsigned arr_size, bool reverse) {
 	copy_array(arr, aux_arr, arr_size);
-	std::cout << std::format("Количество элементов: {}. Заданный массив:", arr_size);
-	display_arr(arr, arr_size);
+	std::cout << std::format("Количество элементов: {}. Заданный массив:{}", arr_size, display_arr(arr, arr_size));
 	std::cout << '.';
 	if (reverse)
 		std::cout << " Сортировка по убыванию" << std::endl;
@@ -22,30 +22,26 @@ void print_result(int arr[], int aux_arr[], unsigned arr_size, bool reverse) {
 				 std::setw(16) << "Перестановок    " << std::endl;
 	copy_array(arr, aux_arr, arr_size);
 	selection_sort(arr, arr_size, reverse);
-
-	std::cout << std::setw(16) << "выбор (n)       " << std::setw(16);
-	display_arr(arr, arr_size);
-	std::cout << std::setw(16) << *comparisons << std::setw(16) << *swaps << std::endl;
-
+	std::cout << std::setw(16) << "выбор (n)       " <<
+				 std::setw(16) << display_arr(arr, arr_size) <<
+				 std::setw(16) << *comparisons <<
+				 std::setw(16) << *swaps << std::endl;
 	selection_sort(arr, arr_size, reverse);
-
-	std::cout << std::setw(16) << "выбор (o)       " << std::setw(16);
-	display_arr(arr, arr_size);
-	std::cout << std::setw(16) << *comparisons << std::setw(16) << *swaps << std::endl;
-
+	std::cout << std::setw(16) << "выбор (o)       " <<
+		std::setw(16) << display_arr(arr, arr_size) <<
+		std::setw(16) << *comparisons <<
+		std::setw(16) << *swaps << std::endl;
 	copy_array(aux_arr, arr, arr_size);
 	bubble_sort(arr, arr_size, reverse);
-
-	std::cout << std::setw(16) << "пузырёк (n)     " << std::setw(16);
-	display_arr(arr, arr_size);
-	std::cout << std::setw(16) << *comparisons << std::setw(16) << *swaps << std::endl;
-
+	std::cout << std::setw(16) << "пузырёк (n)     " <<
+		std::setw(16) << display_arr(arr, arr_size) <<
+		std::setw(16) << *comparisons <<
+		std::setw(16) << *swaps << std::endl;
 	bubble_sort(arr, arr_size, reverse);
-
-	std::cout << std::setw(16) << "пузырёк (o)     " << std::setw(16);
-	display_arr(arr, arr_size);
-	std::cout << std::setw(16) << *comparisons << std::setw(16) << *swaps << std::endl << std::endl;
-
+	std::cout << std::setw(16) << "пузырёк (o)     " <<
+		std::setw(16) << display_arr(arr, arr_size) <<
+		std::setw(16) << *comparisons <<
+		std::setw(16) << *swaps << std::endl << std::endl;
 	copy_array(aux_arr, arr, arr_size);
 }
 
