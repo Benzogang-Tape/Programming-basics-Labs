734dfa9a1b9a0b2dc9dcbe45d22dcf12b418c905
DJ Tape <Dj.Tapee@yandex.ru>
2023-12-29 12:52:26 +0300

    version 1.0.0

--- Lab 6/constants.h
+++ Lab 6/constants.h
@@ -1,10 +1,13 @@
 #pragma once
 
 namespace constants {
-    const int eps[5] = { -2, -3, -4 , -5, -6 };
-    const char pipe{ '|' };
-    const char dash{ '-' };
-    const char plus{ '+' };
-    const unsigned column_count{ 4 };                                 // число столбцов таблицы
-    const unsigned column_width[column_count]{ 12, 18, 18, 10 };   // ширина столбцов таблицы
+    inline const char pipe{ '|' };
+    inline const char dash{ '-' };
+    inline const char plus{ '+' };
+    inline const int eps[5] = { -2, -3, -4 , -5, -6 };
+    inline const unsigned functions_count{ 4 };
+    inline const unsigned column_count{ 4 };
+    inline const unsigned column_width[column_count]{ 12, 18, 18, 10 };
+    inline const char functions_init[functions_count][12] = { "y=x ", "y=sin(22x)", "y=x^4 ", "y=arctg(x)" };
+    inline const char method_name[2][31] = { "прямоугольников", "трапеций" };
 }
