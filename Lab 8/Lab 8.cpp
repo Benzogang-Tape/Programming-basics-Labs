0cbfad38741d4d87bdc86c78e88cfc3f1129f31b
DJ Tape <Dj.Tapee@yandex.ru>
2024-01-09 21:59:51 +0300

    It's works

--- Lab 8/Lab 8.cpp
+++ Lab 8/Lab 8.cpp
@@ -5,13 +5,24 @@ int main() {
 	unsigned N{}, M{}, fixed{}, precision{};
 	std::cout << "¬ведите количество строк и столбцов матрицы: ";
 	std::cin >> N >> M;
-	std::cout << "\n¬ведите формат отображени€ элементов матрицы:\n\n0 - дл€ вывода в экспоненциальном формате\n1 - дл€ вывода в формате с фиксированной точкой\n";
+	std::cout << "\n¬ведите формат отображени€ элементов матрицы:\n\n0 - дл€ вывода в экспоненциальном формате\n1 - дл€ вывода в формате с фиксированной точкой\n" << "\x1b[4A" << "\x1b[46C";
 	std::cin >> fixed;
-	std::cout << "¬ведите требуемую точность(количество знаков после зап€той): ";
+	std::cout << "\x1b[4B" << "¬ведите требуемую точность(количество знаков после зап€той): ";
 	std::cin >> precision;
+	std::cout << std::endl;
 	auto matrix{ std::move(create_matrix2(N, M)) };
 	init_matrix(&matrix, N, M);
 	print_matrix(&matrix, N, M, static_cast<bool>(fixed), precision);
 	delete_matrix(std::move(matrix), N, M);
+
+	double B[constants::static_Matrix_Size][constants::static_Matrix_Size]{ { } };
+	init_matrix(B, constants::static_Matrix_Size);
+	print_matrix(B, constants::static_Matrix_Size, constants::static_Matrix_Size, true, 0);
+
+	std::cout << B << "  " << B[0] << "  " << B[2] << std::endl;
+	std::cout << B[0][0] << "  " << **B << "  " << *B[0] << std::endl;
+	std::cout << *(*(B + 1)) << "  " << *B[1] << std::endl;
+	std::cout << *(B[0] + 1) << "  " << *(*B + 1) << std::endl;
+	std::cout << B[0][20] << "  " << *(B[0] + 20) << "  " << *B[2] << std::endl;
 	return 0;
 }
--- Lab 8/functions.h
+++ Lab 8/functions.h
@@ -11,9 +11,10 @@ double** create_matrix0(const unsigned, const unsigned);
 void create_matrix1(std::unique_ptr<std::unique_ptr<double[]>[]>*, const unsigned, const unsigned);
 std::unique_ptr<std::unique_ptr<double[]>[]> create_matrix2(const unsigned, const unsigned);
 
-void init_matrix(double**, const unsigned, const unsigned);
+void init_matrix(double [constants::static_Matrix_Size][constants::static_Matrix_Size], const unsigned);
 void init_matrix(std::unique_ptr<std::unique_ptr<double[]>[]>*, const unsigned, const unsigned);
 
 void delete_matrix(std::unique_ptr<std::unique_ptr<double[]>[]>, const unsigned, const unsigned);
 
 void print_matrix(const std::unique_ptr<std::unique_ptr<double[]>[]>* const, const unsigned, const unsigned, bool fixed=true, const unsigned precision = constants::default_precision);
+void print_matrix(const double [constants::static_Matrix_Size][constants::static_Matrix_Size], const unsigned, const unsigned, bool fixed = true, const unsigned precision = constants::default_precision);
