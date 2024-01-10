90359d5c16153d037c991df81aed3e2a54668e90
DJ Tape <Dj.Tapee@yandex.ru>
2024-01-10 03:23:23 +0300

    Version 1.0.0

---
+++ Lab 8.1/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace constants {
+	inline constexpr int x{ 1 };
+	inline constexpr size_t max_Line_Length{ 80 };
+	inline constexpr size_t integer_Part{ 4 };
+	inline constexpr size_t N{ 9 };
+	inline constexpr size_t M{ 14 };
+	inline constexpr size_t default_precision{ 7 };
+	inline constexpr size_t static_Matrix_Size{ 10 };
+}
---
+++ Lab 8.1/mprinter.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include<functional>
+#include<iomanip>
+#include"constants.h"
+
+void create_matrix(double**&, const unsigned, const unsigned);
+void init_dymanic_matrix(double**, const unsigned, const unsigned);
+void init_static_matrix(double [][constants::static_Matrix_Size], const unsigned);
+void print_matrix(double*[], const unsigned, const unsigned, bool fixed = true, const unsigned precision = constants::default_precision);
+void delete_matrix(double**&, const unsigned, const unsigned);
+void get_row_pointers(double[][constants::static_Matrix_Size], double* [constants::static_Matrix_Size]);
