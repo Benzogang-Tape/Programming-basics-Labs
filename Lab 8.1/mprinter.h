9a71ded7a2afb232c863ce3b93ad2d1ce4364c43
DJ Tape <Dj.Tapee@yandex.ru>
2024-01-10 03:55:22 +0300

    Version 1.0.2

--- Lab 8.1/mprinter.h
+++ Lab 8.1/mprinter.h
@@ -8,6 +8,6 @@
 void create_matrix(double**&, const unsigned, const unsigned);
 void init_dymanic_matrix(double**, const unsigned, const unsigned);
 void init_static_matrix(double [][constants::static_Matrix_Size], const unsigned);
-void print_matrix(const double* const [], const unsigned, const unsigned, bool fixed = true, const unsigned precision = constants::default_precision);
+void print_matrix(const double* const [], const unsigned, const unsigned, const bool fixed = true, const unsigned precision = constants::default_precision);
 void delete_matrix(double**&, const unsigned, const unsigned);
 void get_row_pointers(double[][constants::static_Matrix_Size], double* [constants::static_Matrix_Size]);
