64cad21923bd2319f674ef254ddfd08353ae0ccf
DJ Tape <Dj.Tapee@yandex.ru>
2024-01-10 03:56:23 +0300

    Version 1.0.0

--- Lab 8/functions.h
+++ Lab 8/functions.h
@@ -6,15 +6,11 @@
 #include<iomanip>
 #include"constants.h"
 
-void create_matrix(double**&, const unsigned, const unsigned);
-double** create_matrix0(const unsigned, const unsigned);
-void create_matrix1(std::unique_ptr<std::unique_ptr<double[]>[]>*, const unsigned, const unsigned);
-std::unique_ptr<std::unique_ptr<double[]>[]> create_matrix2(const unsigned, const unsigned);
 
+std::unique_ptr<std::unique_ptr<double[]>[]> create_matrix(const unsigned, const unsigned);
 void init_matrix(double [constants::static_Matrix_Size][constants::static_Matrix_Size], const unsigned);
 void init_matrix(std::unique_ptr<std::unique_ptr<double[]>[]>*, const unsigned, const unsigned);
-
-void delete_matrix(std::unique_ptr<std::unique_ptr<double[]>[]>, const unsigned, const unsigned);
-
 void print_matrix(const std::unique_ptr<std::unique_ptr<double[]>[]>* const, const unsigned, const unsigned, bool fixed=true, const unsigned precision = constants::default_precision);
-void print_matrix(const double [constants::static_Matrix_Size][constants::static_Matrix_Size], const unsigned, const unsigned, bool fixed = true, const unsigned precision = constants::default_precision);
+void print_matrix(const double* const [], const unsigned, const unsigned, const bool fixed = true, const unsigned precision = constants::default_precision);
+void delete_matrix(std::unique_ptr<std::unique_ptr<double[]>[]>, const unsigned, const unsigned);
+void get_row_pointers(double[][constants::static_Matrix_Size], double* [constants::static_Matrix_Size]);
