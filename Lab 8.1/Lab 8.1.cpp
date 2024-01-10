0ca6c99194222f9f8afc7ab36ec48a2a1b8d3417
DJ Tape <Dj.Tapee@yandex.ru>
2024-01-10 03:23:23 +0300

    Version 1.0.0

---
+++ Lab 8.1/Lab 8.1.cpp
@@ -0,0 +1,35 @@
+#include "mprinter.h"
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	unsigned N{}, M{}, fixed{}, precision{};
+	std::cout << "¬ведите количество строк и столбцов матрицы: ";
+	std::cin >> N >> M;
+	std::cout << "\n¬ведите формат отображени€ элементов матрицы:\n\n0 - дл€ вывода в экспоненциальном формате\n1 - дл€ вывода в формате с фиксированной точкой\n" << "\x1b[4A" << "\x1b[46C";
+	std::cin >> fixed;
+	std::cout << "\x1b[4B" << "¬ведите требуемую точность(количество знаков после зап€той): ";
+	std::cin >> precision;
+	std::cout << std::endl;
+
+	double** matrix = new double* [N];
+	create_matrix(matrix, N, M);
+	init_dymanic_matrix(matrix, N, M);
+	print_matrix(matrix, N, M, static_cast<bool>(fixed), precision);
+	delete_matrix(matrix, N, M);
+
+	double B[constants::static_Matrix_Size][constants::static_Matrix_Size]{ { } };
+	double* row_pointers [constants::static_Matrix_Size];
+	init_static_matrix(B, constants::static_Matrix_Size);
+	get_row_pointers(B, row_pointers);
+	print_matrix(row_pointers, constants::static_Matrix_Size, constants::static_Matrix_Size, true, 0);
+
+	/*
+	std::cout << B << "  " << B[0] << "  " << B[2] << std::endl;
+	std::cout << B[0][0] << "  " << **B << "  " << *B[0] << std::endl;
+	std::cout << *(*(B + 1)) << "  " << *B[1] << std::endl;
+	std::cout << *(B[0] + 1) << "  " << *(*B + 1) << std::endl;
+	std::cout << B[0][20] << "  " << *(B[0] + 20) << "  " << *B[2] << std::endl;
+	*/
+
+	return 0;
+}
---
+++ Lab 8.1/mprinter.cpp
@@ -0,0 +1,172 @@
+#include "mprinter.h"
+
+std::function<unsigned(unsigned)> fact{ [](unsigned n) -> unsigned { if (n == 1 or n == 0) return 1; else return n * fact(n - 1); } };
+
+void create_matrix(double**& matrix, const unsigned N, const unsigned M) {
+	for (unsigned i{}; i < N; ++i) {
+		matrix[i] = new double[M];
+	}
+}
+
+
+void init_dymanic_matrix(double** matrix, const unsigned N, const unsigned M) {
+	for (size_t i{}; i < N; i++) {
+		for (size_t j{}; j < M; j++) {
+			if (i < j) {
+				if ((j - i) > 1)
+					matrix[i][j] = (matrix[i][j - 1] / pow(j + 1, i + 1));
+				else
+					matrix[i][j] = (pow(constants::x, i + 1) / pow(fact(j + 1), i + 1));
+			}
+			else if (i > j) {
+				if (j)
+					matrix[i][j] = matrix[i][j - 1] / pow(i + 1, j + 1);
+				else
+					matrix[i][j] = pow(-constants::x, i + 1) / pow(fact(j + 1), i + 1);
+			}
+			else {
+				matrix[i][j] = 1;
+			}
+		}
+	}
+}
+
+
+void init_static_matrix(double matrix[][constants::static_Matrix_Size], const unsigned N) {
+	for (unsigned i{}; i < N; ++i) {
+		for (unsigned j{}; j < constants::static_Matrix_Size; ++j) {
+			matrix[i][j] = i * 10 + j;
+		}
+	}
+}
+
+
+void delete_matrix(double**& matrix, const unsigned N, const unsigned M) {
+	for (unsigned i{}; i < N; ++i) {
+		delete [] matrix[i];
+	}
+	delete[] matrix;
+	matrix = nullptr;
+}
+
+
+void get_row_pointers(double matrix[][constants::static_Matrix_Size], double* row_pointers[constants::static_Matrix_Size]) {
+	for (unsigned i{}; i < constants::static_Matrix_Size; ++i)
+		row_pointers[i] = matrix[i];
+}
+
+
+void print_matrix(double* matrix[], const unsigned N, const unsigned M, bool fixed, const unsigned precision) {
+	unsigned column_Count{ constants::max_Line_Length / (constants::integer_Part + precision) };
+	std::cout << std::fixed << std::setprecision(precision);
+	unsigned width{ constants::integer_Part + precision };
+	if (!fixed) {
+		std::cout << std::scientific;
+		column_Count = constants::max_Line_Length / (constants::integer_Part + precision + 5);
+		width += 5;
+	}
+	if (column_Count >= M)
+		column_Count = M;
+	std::cout << std::setfill('=') << std::setw(constants::max_Line_Length) << ' ' << std::setfill(' ');
+	static unsigned columns_Printed{};
+	unsigned current_Columns_Count{};
+	while (columns_Printed < M) {
+		if (columns_Printed)
+			std::cout << std::setfill('-') << std::setw(constants::max_Line_Length) << ' ' << std::setfill(' ');
+		current_Columns_Count = [M, column_Count]() -> unsigned { if ((M - columns_Printed) / column_Count) return columns_Printed + column_Count; return columns_Printed + (M % column_Count); }();
+		for (unsigned i{}; i < N; ++i) {
+			for (unsigned j{ columns_Printed }; j < current_Columns_Count; ++j) {
+				std::cout << std::setw(width) << matrix[i][j];
+			}
+			std::cout << std::endl;
+		}
+		columns_Printed += column_Count;
+	}
+	std::cout << std::setfill('=') << std::setw(constants::max_Line_Length) << ' ' << std::setfill(' ') << std::endl;
+	columns_Printed = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Creating two-dimensional matrix function (default, with passing the pointer to the matrix to the function by reference)
+/*
+void create_matrix(double **&matrix, const unsigned N, const unsigned M) {
+	for (unsigned i{}; i < N; ++i) {
+		matrix[i] = new double[M];
+	}
+}
+
+usage:
+	double** matrix{new double* [constants::N]};
+	create_matrix(matrix, constants::N, constants::M);
+
+*/
+
+
+//Creating two-dimensional matrix function (default, with using relocation semantics to the pointer to matrix that function returns)
+/*
+double** create_matrix0(const unsigned N, const unsigned M) {
+	double** matrix = new double* [N];
+	for (unsigned i{}; i < N; ++i) {
+		matrix[i] = new double[M];
+	}
+	return matrix;
+}
+
+usage:
+		double** matrix = std::move(create_matrix0(constants::N, constants::M));
+
+*/
+
+
+//Creating two-dimensional matrix function (with smart-pointers and passing the smart-pointer to the matrix to the function by pointer)
+/*
+
+void create_matrix1(std::unique_ptr<std::unique_ptr<double[]>[]> *matrix, const unsigned N, const unsigned M) {
+	for (size_t i{}; i < N; ++i) {
+		matrix->get()[i] = std::make_unique<double[]>(M);
+	}
+}
+
+usage:
+	auto matrix{ std::make_unique<std::unique_ptr<double[]>[]>(constants::N) };
+	create_matrix1(&matrix, constants::N, constants::M);
+
+*/
+
+
+//Creating two-dimensional matrix function (with smart-pointers and using relocation semantics to the smart-pointer to matrix that function returns)
+/*
+
+std::unique_ptr<std::unique_ptr<double[]>[]> create_matrix2(const unsigned N, const unsigned M) {
+	std::unique_ptr<std::unique_ptr<double[]>[]> matrix{ std::make_unique<std::unique_ptr<double[]>[]>(N) };
+	for (size_t i{}; i < N; ++i) {
+		matrix.get()[i] = std::make_unique<double[]>(M);
+	}
+	return matrix;
+}
+
+usage:
+	auto matrix{ std::move(create_matrix2(constants::N, constants::M)) };
+	std::unique_ptr<std::unique_ptr<double[]>[]> matrix{ std::move(create_matrix2(constants::N, constants::M)) };
+
+*/
