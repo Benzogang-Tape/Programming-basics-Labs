6a2469e4ea1aa221f757d980516073423a508a39
DJ Tape <Dj.Tapee@yandex.ru>
2024-07-15 01:02:08 +0300

    Chagne encoding to UTF-8

--- Lab 6/Lab 6.cpp
+++ Lab 6/Lab 6.cpp
@@ -8,12 +8,12 @@ int main() {
 	unsigned n{ 1 };
 	const func_ref_1 functions[4]{ F_x, F_sin, F_pow_x, F_arctan };
 	const func_ref_2 functions_exact[4]{ X_exact, Sin_exact, PowX_exact, Arctan_exact };
-	const func_ref_3 Int—alculationFuncs[2]	{ IntRect, IntTrap };
+	const func_ref_3 Int–°alculationFuncs[2]	{ IntRect, IntTrap };
 	I_print i_pr[4] = { {} };
 
-	std::cout << "¬‚Â‰ËÚÂ ÌËÊÌ˛˛ „‡ÌËˆÛ ËÌÚÂ„ËÓ‚‡ÌËˇ: ";
+	std::cout << "–í–≤–µ–¥–∏—Ç–µ –Ω–∏–∂–Ω—é—é –≥—Ä–∞–Ω–∏—Ü—É –∏–Ω—Ç–µ–≥—Ä–∏—Ä–æ–≤–∞–Ω–∏—è: ";
 	std::cin >> a;
-	std::cout << "¬‚Â‰ËÚÂ ‚ÂıÌ˛˛ „‡ÌËˆÛ ËÌÚÂ„ËÓ‚‡ÌËˇ: ";
+	std::cout << "–í–≤–µ–¥–∏—Ç–µ –≤–µ—Ä—Ö–Ω—é—é –≥—Ä–∞–Ω–∏—Ü—É –∏–Ω—Ç–µ–≥—Ä–∏—Ä–æ–≤–∞–Ω–∏—è: ";
 	std::cin >> b;
 	
 	for (unsigned j{}; j < 4; j++) {
@@ -21,14 +21,14 @@ int main() {
 		i_pr[j].exact_value = functions_exact[j](a, b);
 	}
 	for (unsigned k{}; k < 2; k++) {
-		std::cout << std::setfill('_') << "\n" << std::setw(25) << "ÃÂÚÓ‰ " << method_name[k] << std::setw(20) << ' ' << std::endl;
+		std::cout << std::setfill('_') << "\n" << std::setw(25) << "–ú–µ—Ç–æ–¥ " << method_name[k] << std::setw(20) << ' ' << std::endl;
 		for (unsigned i{}; i < 5; i++)
 		{
 			std::cout << "\neps = " << std::setprecision(abs(eps[i])) << pow(10, eps[i]) << std::endl;
 			for (unsigned j{}; j < 4; j++)
 			{
 				n = 1;
-				i_pr[j].integral_sum = Int—alculationFuncs[k](functions[j], a, b, pow(10, eps[i]), n);
+				i_pr[j].integral_sum = Int–°alculationFuncs[k](functions[j], a, b, pow(10, eps[i]), n);
 				i_pr[j].n = n;
 			}
 			PrintTabl(i_pr, functions_count);
